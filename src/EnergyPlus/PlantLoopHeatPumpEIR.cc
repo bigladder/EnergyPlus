1406b8d3c7c828c4775980829b8929770a59f3a0
kevin-moos <kevin.moos@bigladdersoftware.com>
2026-07-15 16:42:27 -0500

    Update EIRPlantLoopHeatPump::calcPowerUsage to use referenceCapacity instead of loadSideHeatTransfer

--- src/EnergyPlus/PlantLoopHeatPumpEIR.cc
+++ src/EnergyPlus/PlantLoopHeatPumpEIR.cc
@@ -752,8 +752,7 @@ void EIRPlantLoopHeatPump::calcPowerUsage(EnergyPlusData &state)
 
     // compute power usage
     if (this->thermosiphonDisabled(state)) {
-        this->powerUsage =
-            (this->loadSideHeatTransfer / this->referenceCOP) * eirModifierFuncPLR * eirModifierFuncTemp * this->defrostPowerMultiplier;
+        this->powerUsage = (this->referenceCapacity / this->referenceCOP) * eirModifierFuncPLR * eirModifierFuncTemp * this->defrostPowerMultiplier;
     }
 }
 
